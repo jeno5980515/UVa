1ffe3cc09ce6758b9a540bd4f7e5701c42481cd4
jeno5980515 <jeno5980515@yahoo.com.tw>
2017-09-07 15:04:03 +0800

    modify 12621

--- 12621 - On a Diet.cpp
+++ 12621 - On a Diet.cpp
@@ -6,22 +6,24 @@ int main(){
   int caseNumber, itemAmount, cal, item;
   cin >> caseNumber ;
   while(caseNumber--){
-    bool dp[10000] = {};
+    bool dp[1000] = {};
     dp[0] = true;
     cin >> cal >> itemAmount ;
+    cal /= 10;
     for (int i = 0; i < itemAmount; i ++){
       cin >> item;
-      for(int j = 10000-item; j >= 0; j --) {
+      item /= 10;
+      for(int j = 1000-item; j >= 0; j --) {
         if (dp[j]){
           dp[j+item] = true;
         }
       }
     }
     int resultIndex = cal;
-    while( resultIndex < 10000 && dp[resultIndex] == false )
+    while( resultIndex < 1000 && dp[resultIndex] == false )
       resultIndex ++;
-    if ( resultIndex != 10000 ){
-      cout << resultIndex << endl ;
+    if ( resultIndex != 1000 ){
+      cout << resultIndex * 10 << endl ;
     } else {
       cout << "NO SOLUTION" << endl ;
     }
